3d932020f654a3ec3a9249b49cacb66931460d07
deviozc <deviozc@gmail.com>
2014-12-08 02:31:08 +0000

    Fixed error handling on uncaught exception

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -12,7 +12,9 @@ var path = require("path");
 module.exports.bootstrap = function(cb) {
   sails.config.paths.public = path.resolve(sails.config.appPath, "assets");
   process.on('uncaughtException', function (err) {
-    console.log(err);
+    console.error('uncaught error');
+    console.error(err);
+    process.exit(1);
   })
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
