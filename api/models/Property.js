4eeec0365c9e3c7e162903ffc24eb9d5b0bf2a9b
deviozc <deviozc@gmail.com>
2014-12-15 06:21:47 +0000

    Removed required flag of property sysid

--- api/models/Property.js
+++ api/models/Property.js
@@ -8,7 +8,7 @@ module.exports = {
     attributes: {
         sysid: {
             type: "integer",
-            required: true
+            required: false
         },
         type: {
             type: "string",
