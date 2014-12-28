2de462f076b0b820d506936b1821a0623772ed6a
deviozc <deviozc@gmail.com>
2014-12-28 03:46:31 +0000

    Updated image path

--- config/constants.js
+++ config/constants.js
@@ -10,7 +10,7 @@ module.exports.constants = {
     // 240000 => 4 minutes (240,000 miliseconds)
     uploadTimeout: 120000,
     uploadMaxBytes: 10000000,
-    baseAssetsDirectory: "/var/www/lavamelon.com/cms-core/assets/uploads/",
+    baseAssetsDirectory: "/var/www/lavamelon.com/public_html/assets/uploads",
     baseAssetsURL: "www.lavamelon.com/assets/uploads/",
     basePropertyImageURL: "http://www.lavamelon.com/mls/mls-images",
     
